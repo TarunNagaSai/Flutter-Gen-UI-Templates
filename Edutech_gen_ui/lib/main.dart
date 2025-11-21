15b37c7a2503a127043c5408f6dd952898a94d62
tarun-naga-sai <tarunnagasai007@gmail.com>
2025-11-21 19:43:18 +0530

    logger integrated to the GUI

--- Edutech_gen_ui/lib/main.dart
+++ Edutech_gen_ui/lib/main.dart
@@ -6,23 +6,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:education_gen_ui/src/const/theme.dart';
+import 'package:genui/genui.dart';
+import 'package:logging/logging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   LocalStorageService().init();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: ".env");
+  configureGenUiLogging(level: Level.ALL);
   runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final appRouter = AppRouter();
-    
+
     return MaterialApp.router(
       title: 'Edutech Gen UI',
       routerConfig: appRouter.config(),
